a18cf921081aeb265261126f80b46fba0d78111f
GabeSvbr <gabrielveber193@gmail.com>
2026-07-22 17:37:35 -0300

    Refactor Windows Manager for improved readability
    Refactor Windows Manager script to improve readability and functionality. Updated color formatting, input handling, and menu structure.

--- Windows_Mng.py
+++ Windows_Mng.py
@@ -20,19 +20,19 @@ def cmenu_out2():
 def cdots():
     play_sound("/usr/share/sounds/ocean/stereo/completion-success.oga",70)
 
-# Colors
-
-def green(text): print(f"\033[1;38;5;120m{text}\033[0m")
-def orange(text):print(f"\033[1;38;5;208m{text}\033[0m")
-def red(text):print(f"\033[31m{text}\033[0m")
-
 # little def's
 
 def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
 def get_option():
-    val = int(input("    \033[1;38;5;208mOption: \033[0m")or 0)
-    return val
+    while True:
+        input = input("    \033[1;38;5;208mOption: \033[0m").strip()
+        if input == "":
+            return 0
+        try:
+            return int(entrada)
+        except ValueError:
+            valid()
 def confirmation():
     resposta = input("     \033[32mcontinue...\033[0m")
 def bar():
@@ -53,21 +53,21 @@ def menu_spacing():
 #  Windows Section
 
 def windows_main_menu_print():
-    clear_console();        print(f"\033[1;38;5;208m  ----< {time.strftime('%H:%M')} >----< Pinalto's Windows Manager >-------\033[0m");    bar()
-    print("\033[1m |  \033[38;5;208m1 ➜\033[0m \033[1;36mComplete System Update\033[0m                          |\033[0m")
-    print("\033[1m |  \033[38;5;208m2 ➜\033[0m \033[1;36mSetup Options\033[0m                                   |\033[0m")
-    print("\033[1m |  \033[38;5;208m3 ➜\033[0m \033[1;36mList Machine Components\033[0m                         |\033[0m")
-    print("\033[1m |  \033[38;5;208m4 ➜\033[0m \033[1;36mGithub Repos\033[0m                                    |\033[0m")
-    print("\033[1m |  \033[38;5;208m5 ➜\033[0m \033[1;36mPower Options\033[0m                                   |\033[0m")
-    print("\033[1m |  \033[38;5;208m0 ➜\033[0m \033[1;31mQuit\033[0m                                            |\033[0m");  bar()
-
+    clear_console(); print(f"\033[1;38;2;124;77;255m  ----< {time.strftime('%H:%M')} >----< Pinalto's Windows Manager >-------\033[0m"); bar()
+    print("\033[1m |  \033[1;38;2;124;77;255m1 ➜\033[0m \033[1;38;2;216;200;255mComplete System Update\033[0m                          |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m2 ➜\033[0m \033[1;38;2;216;200;255mSetup Options\033[0m                                   |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m3 ➜\033[0m \033[1;38;2;216;200;255mList Machine Components\033[0m                         |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m4 ➜\033[0m \033[1;38;2;216;200;255mLink Manager\033[0m                                    |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m9 ➜\033[0m \033[1;38;2;216;200;255mShutdown /s /f /t 0\033[0m                             |\033[0m")
+    print("\033[1m |  \033[1;38;2;255;107;107m0 ➜\033[0m \033[1;38;2;255;107;107mQuit\033[0m                                            |\033[0m")
+    bar()
 
 # Option 1 System Update
 
 def update():
     
     t = time.time()
-    cmenu_in(); clear_console(); orange("             ----- Updating System -----  "); bar(); time.sleep(0.4)
+    clear_console(); print("             ----- Updating System -----  "); bar(); time.sleep(0.4)
     cmd = (
         'winget upgrade --all --silent --accept-source-agreements --accept-package-agreements && '
         'DISM /Online /Cleanup-Image /StartComponentCleanup /ResetBase && '
@@ -75,22 +75,23 @@ def update():
     )
     try:
         subprocess.run(cmd, shell=True, check=True)
-        green("[ok] update + cleanup")
+        print("[ok] update + cleanup")
     except subprocess.CalledProcessError:
-        red("[erro] update + cleanup")
+        print("[erro] update + cleanup")
     
     bar(); print(f"\033[1;93mElapsed time: {time.time() - t:.4f}\033[0m")
     bar(); confirmation()
 
 # Option 2 Setup Options
 def windows_opc3_menu_print():
-    clear_console();        print(f"\033[1;38;5;208m                 Setup Menu...\033[0m"); bar()
-    print("\033[1m |  \033[38;5;208m1 ➜\033[0m \033[1;36mDownload Utilitaries Packages\033[0m                   |\033[0m")
-    print("\033[1m |  \033[38;5;208m2 ➜\033[0m \033[1;36mDownload Gaming Packages\033[0m                        |\033[0m")
-    print("\033[1m |  \033[38;5;208m3 ➜\033[0m \033[1;36mDownload Worktools Packages\033[0m                     |\033[0m")
-    print("\033[1m |  \033[38;5;208m4 ➜\033[0m \033[1;36mDownload All Packages\033[0m                           |\033[0m")
-    print("\033[1m |  \033[38;5;208m5 ➜\033[0m \033[1;36mPackages Info:\033[0m                                  |\033[0m")
-    print("\033[1m |  \033[38;5;208m0 ➜\033[0m \033[1;31mLeave\033[0m                                           |\033[0m");   bar()
+    clear_console(); print(f"\033[1;38;2;124;77;255m                 Setup Menu...\033[0m"); bar()
+    print("\033[1m |  \033[1;38;2;124;77;255m1 ➜\033[0m \033[1;38;2;216;200;255mDownload Utilities Packages\033[0m                    |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m2 ➜\033[0m \033[1;38;2;216;200;255mDownload Gaming Packages\033[0m                       |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m3 ➜\033[0m \033[1;38;2;216;200;255mDownload Work Tools Packages\033[0m                   |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m4 ➜\033[0m \033[1;38;2;216;200;255mDownload All Packages\033[0m                          |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m5 ➜\033[0m \033[1;38;2;216;200;255mPackages Info\033[0m                                  |\033[0m")
+    print("\033[1m |  \033[1;38;2;255;107;107m0 ➜\033[0m \033[1;38;2;255;107;107mLeave\033[0m                                         |\033[0m")
+    bar()
 
 
 def install(package_id):
@@ -118,11 +119,11 @@ def windows_download_utilitaries():
 
 def windows_download_gaming():
     install("Valve.Steam")
+    install("Discord.Discord")
     install("PrismLauncher.PrismLauncher")
     install("CPUID.CPU-Z")
     install("YoutubeMusicDesktopApp.YoutubeMusicDesktopApp")
     install("MullvadVPN.MullvadVPN")
-    install("Discord.Discord")
     install("Logitech.GHUB")
     install("Guru3D.Afterburner")
     install("EpicGames.EpicGamesLauncher")
@@ -146,18 +147,19 @@ def windows_download_all():
 
 
 def windows_show_packages():
-    orange("         === UTILITARIES ===")
-    green("--> winget: Git, Python, 7zip, LibreOffice, VLC, Speedtest, PowerToys, Bazaar, Helium Browser")
-    orange("\n       === GAMING ===")
-    green("--> winget: Steam, PrismLauncher, MangoHud, YouTube Music Desktop, ProtonVPN")
-    orange("\n       === WORKTOOLS ===")
-    green("--> winget: VSCode, Git, Python, Wget, Vim, OBS Studio, Krita, Nmap, Vesktop, PyCharm Community, VirtualBox")
+    print("\033[1;38;2;124;77;255m         === UTILITIES ===\033[0m")
+    print("\033[1;38;2;216;200;255m--> winget: Git, Python, WinRAR, LibreOffice, VLC, Helium Browser, Brave Browser, WizTree, Rufus, Kate\033[0m")
+
+    print("\n\033[1;38;2;124;77;255m         === GAMING ===\033[0m")
+    print("\033[1;38;2;216;200;255m--> winget: Steam, Prism Launcher, CPU-Z, YouTube Music Desktop, Mullvad VPN, Playnite, WeMod, Epic Games, Discord\033[0m")
+
+    print("\n\033[1;38;2;124;77;255m        === WORK TOOLS ===\033[0m")
+    print("\033[1;38;2;216;200;255m--> winget: Visual Studio Code, Git, Python, Wget, OBS Studio, Nmap, HandBrake\033[0m")
 
 
 def windows_setup():
-    cmenu_in()
     while True:
-        cachy_opc3_menu_print()
+        windows_opc3_menu_print()
         try:
             opc = get_option()
         except ValueError:
@@ -167,109 +169,83 @@ def windows_setup():
         elif opc == 3: windows_download_worktools(); all_done()
         elif opc == 4: windows_download_all()
         elif opc == 5: windows_show_packages(); confirmation()
-        else: cmenu_out(); break
+        else: break
 
 # Option 3 Components Listing
 
 def windows_list_components():
-    clear_console()
-    orange("                  --- COMPONENTS ---                          ")
-    bar()
+    clear_console();print("\n\033[1;38;2;124;77;255m                  --- COMPONENTS ---                          \033[0m");   bar()
     time_start = time.time()
     os.system('powershell -Command "Get-ComputerInfo"')
     time_end = time.time()
+    bar();  print(f"\033[1;93mElapsed time: {time_end - time_start:.4f}\033[0m")
+    bar();  confirmation()
+
+
+# Option 4 Link Manager
+
+def links_manager_print():
+    clear_console(); print(f"\033[1;38;2;124;77;255m                Link Manager...\033[0m"); bar()
+    print("\033[1m |  \033[1;38;2;124;77;255m1 ➜\033[0m \033[1;38;2;216;200;255mPinalto's PcManager\033[0m                             |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m2 ➜\033[0m \033[1;38;2;216;200;255mFsOS Homepage\033[0m                                   |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m3 ➜\033[0m \033[1;38;2;216;200;255mSilent Hill Native PC (Linux/Win) (.ISO needed)\033[0m |\033[0m")
+    print("\033[1m |  \033[1;38;2;124;77;255m4 ➜\033[0m \033[1;38;2;216;200;255mSteam Achievement Unlocker\033[0m                      |\033[0m")
+    print("\033[1m |  \033[1;38;2;255;107;107m0 ➜\033[0m \033[1;38;2;255;107;107mLeave\033[0m                                          |\033[0m")
     bar()
-    print(f"\033[1;93mElapsed time: {time_end - time_start:.4f}\033[0m")
-    bar()
-    confirmation()
-
-
-# Option 4 Repo Manager
-
-def repo_manager_print():
-    clear_console();    print(f"\033[1;38;5;208m                Github Repos...\033[0m"); bar()
-    print("\033[1m |  \033[38;5;208m1 ➜\033[0m \033[1;36mSilent Hill Native PC (linux/win)(.iso needed)\033[0m  |\033[0m");
-    print("\033[1m |  \033[38;5;208m2 ➜\033[0m \033[1;36mSteam Achivement Unlocker\033[0m                       |\033[0m");
-    print("\033[1m |  \033[38;5;208m3 ➜\033[0m \033[1;36mPinalto's PcManager\033[0m                             |\033[0m");
-    print("\033[1m |  \033[38;5;208m0 ➜\033[0m \033[1;31mLeave\033[0m                                           |\033[0m");  bar()
-def repo_manager():
-    cmenu_in()
+def links_manager():
     while True:
-        repo_manager_print()
+        links_manager_print()
         opc = get_option()
-        if opc == 1:    webbrowser.open("https://github.com/SlickAmogus/silent-hill-pc-nightly")
-        elif opc == 2:  webbrowser.open("https://github.com/asdfghj1237890/SteamAchievementManager-Enhanced")
-        elif opc == 3:  webbrowser.open("https://github.com/GabeSvbr/Pinaltos_PcManager")
+        if opc == 1:    webbrowser.open("https://github.com/GabeSvbr/Pinaltos_PcManager")
+        elif opc == 2:  webbrowser.open("https://fsosx.com/")
+        elif opc == 3:  webbrowser.open("https://github.com/SlickAmogus/silent-hill-pc-nightly")
+        elif opc == 4:  webbrowser.open("https://github.com/asdfghj1237890/SteamAchievementManager-Enhanced")
         else: break
 
-# Option 5 Power Menu
-def power_menu_print():
-    clear_console();        print("\033[38;5;208m             ----- Power Options -----  \033[0m"); bar()
-    print("\033[1m |  \033[38;5;208m1 ➜\033[0m \033[1;36mPower Off\033[0m                                       |\033[0m");
-    print("\033[1m |  \033[38;5;208m2 ➜\033[0m \033[1;36mReboot\033[0m                                          |\033[0m");
-    print("\033[1m |  \033[38;5;208m3 ➜\033[0m \033[1;36mSuspend\033[0m                                         |\033[0m");
-    print("\033[1m |  \033[38;5;208m0 ➜\033[0m \033[1;31mLeave\033[0m                                           |\033[0m");  bar()
+# Option 9 Full-Shutdown
+def shutdown():
+    os.system("shutdown /s /f /t 0")
 
-def linux_power_menu():
-    cmenu_in();
-    while True:
-        power_menu_print()
-        try:    opc = get_option()
-        except ValueError:      valid();        continue
-        if opc == 1:
-            opc = input("      \033[31mAre you sure \033[32m(y/n)\033[31m: \033[0m")
-            if opc == "y":      os.system("shutdown now")
-            else:       clear_console();    leave_menu()
-        elif opc == 2:
-            opc = input("      \033[31mAre you sure \033[32m(y/n)\033[31m: \033[0m")
-            if opc == "y":      os.system("reboot")
-            else:       clear_console();    leave_menu()
-        elif opc == 3:      os.system("systemctl suspend")
-        else: cmenu_out();   break
-#  Debug menu
 
 def menu_debug():
     while True:
-        clear_console();print(f"1 - cmenu_in\n2 - cmenu_in2\n3 - cmenu_out\n4 - cmenu_out2\n5 - cdots")
+        clear_console();
+        print(f"1 - cmenu_in\n2 - cmenu_in2\n3 - cmenu_out\n4 - cmenu_out2\n5 - cdots")
+        windows_main_menu_print()
         opc=get_option()
         if opc == 1: cmenu_in()
         elif opc == 2: cmenu_in2()
         elif opc == 3: cmenu_out()
         elif opc == 4: cmenu_out2()
         elif opc == 5: cdots()
-        else: cmenu_out();break
+        else:break
 
 # Main Navigator
 
 def menu_windows():
     cont1 = 0
-    cmenu_in2()
     while cont1 == 0:
         windows_main_menu_print()
         try: opc = get_option()
         except ValueError: valid(); continue
-        if opc == 1:
-            t = time.time(); clear_console(); Syu()
-            bar(); print(f"\033[1;93mElapsed time: {time.time() - t:.4f}\033[0m")
-            bar(); confirmation();cmenu_out()
+        if opc == 1: update()
         elif opc == 2: windows_setup()
         elif opc == 3: windows_list_components()
-        elif opc == 4: repo_manager()
-        elif opc == 5: linux_power_menu()
+        elif opc == 4: links_manager()
+        elif opc == 9: shutdown()
         elif opc == 99: menu_debug()
         else: cont1 += 1; clear_console()
 
 #    -MAIN-
 
 def main():
-    orange("Pinalto's Manager...\n")
+    print("\n\033[1;38;2;124;77;255m Pinalto's Manager  '\033[0m")
     for i in range(4):
         clear_console()
         bar()
         print(f"\033[1m |\033[1;34mLoading CachyOS Version{'.' * i}\033[0m")
         bar()
-        cdots()
-        time.sleep(0.20)
+        time.sleep(0.15)
     menu_windows()
 
 #===================================================== RUN ====================================================#"
